5569c208f444d7cf9ec895bdd4bb020b347a856e
afzalansari12 <afzalansari12ab@gmail.com>
2026-04-06 21:52:40 +0530

    faulty calculator

--- 46-faulty_calculator/script.js
+++ 46-faulty_calculator/script.js
@@ -1,1 +1,23 @@
-console.log("faulty calculator")
+const prompt = require("prompt-sync")();
+
+console.log("faulty calculator");
+
+let random = Math.random();
+
+let a = prompt("Enter first number: ");
+let c = prompt("Enter operation: ");
+let b = prompt("Enter second number: ");
+
+let obj = {
+  "+": "-",
+  "*": "+",
+  "-": "/",
+  "/": "**"
+};
+
+if (random > 0.1) {
+  console.log(`The result is ${eval(`${a}${c}${b}`)}`);
+} else {
+  c = obj[c];
+  console.log(`The result is ${eval(`${a}${c}${b}`)}`);
+}
